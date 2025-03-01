f34a482c28db0854113d6dbe1dbb4b825fa25f2c
ivinay1 <joshivinay822@gmail.com>
2025-03-01 10:35:17 +0530

    second commit

---
+++ Basics/HelloWorld.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+int main()
+{
+    std::cout<<"Hello World"<<'\n';
+    std::cout<<"Hello World"<<std::endl;
+
+    // This is single line comment
+
+    /*
+    This is multiple line comment
+    */
+
+    // "endl" and "\n" are both used to put the cursor to the next line but endl flushes outs the output (memory) buffer while "\n" do not does such thing that's why "\n" is preferred over "endl"
+
+    return 0;
+}
